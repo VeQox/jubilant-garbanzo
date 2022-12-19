508513c501bbc657ab8e2a602af032992a06675b
VeQox <sonnleitner.luki@gmail.com>
2022-12-19 17:47:38 +0100

    client server communication ("demo game")

--- client/Program.cs
+++ client/Program.cs
@@ -2,7 +2,70 @@
 
 ClientWebSocket ws = new ClientWebSocket();
 await ws.ConnectAsync(
-    new Uri("ws://localhost:8080")
-    , new CancellationToken());
+    new Uri("ws://localhost:8080"),
+    new CancellationToken());
 
-await ws.SendAsync(new ArraySegment<byte>(new byte[]{(byte)'H',(byte)'e',(byte)'l',(byte)'l',(byte)'o',}), WebSocketMessageType.Text, true, new CancellationToken());
+Thread inputThread = new(() =>
+{
+  while (true)
+  {
+    if (Console.KeyAvailable)
+    {
+      var key = Console.ReadKey(true);
+      ws.SendAsync(new ArraySegment<byte>(ConvertToByteArray(key.Key.ToString())), WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+  }
+});
+
+inputThread.Start();
+
+var buffer = new byte[1024];
+var result = ConvertToString(buffer, await ws.ReceiveAsync(new ArraySegment<byte>(buffer), new CancellationToken()));
+Console.WriteLine(result);
+
+int width = int.Parse(result.Split(";")[0]);
+int height = int.Parse(result.Split(";")[1]);
+
+Console.SetWindowSize(width, height);
+Console.SetBufferSize(width, height);
+Console.CursorVisible = false;
+
+while (true)
+{
+  result = ConvertToString(buffer, await ws.ReceiveAsync(new ArraySegment<byte>(buffer), new CancellationToken()));
+  string[] positions = result.Split(";");
+  Console.Clear();
+  foreach (string position in positions)
+  {
+    try
+    {
+      int x = int.Parse(position.Split("|")[0]);
+      int y = int.Parse(position.Split("|")[1]);
+      Console.SetCursorPosition(x, y);
+      Console.Write("x");
+    }
+    catch
+    {
+
+    }
+  }
+  //await ws.SendAsync(new ArraySegment<byte>(ConvertToByteArray("")), WebSocketMessageType.Text, true, CancellationToken.None);
+}
+
+byte[] ConvertToByteArray(string message)
+{
+  List<byte> encodedMessage = new();
+  foreach (char ch in message)
+    encodedMessage.Add(((byte)ch));
+  return encodedMessage.ToArray();
+}
+
+string ConvertToString(byte[] buffer, WebSocketReceiveResult result)
+{
+  string parsedString = "";
+  for (int i = 0; i < result.Count; i++)
+  {
+    parsedString += ((char)buffer[i]);
+  }
+  return parsedString;
+}
--- server/server.ts
+++ server/server.ts
@@ -1,21 +1,74 @@
-import {IncomingMessage} from "http"
-import { WebSocket, RawData, WebSocketServer} from "ws"
+import { IncomingMessage } from "http"
+import { WebSocket, RawData, WebSocketServer } from "ws"
 
 const port = 8080;
+const width = 100;
+const height = 40;
 
-const wss : WebSocketServer = new WebSocket.Server({port});
+const wss: WebSocketServer = new WebSocket.Server({ port });
+
+interface Connection {
+    ws: WebSocket,
+    ms: number,
+    lastPackage: number,
+    x: number;
+    y: number;
+};
+
+let connections: Connection[] = [];
 
 wss.on("listening", () => {
     console.log(`Server is listening on port ${port}`);
 });
 
-wss.on("connection", (ws : WebSocket, request : IncomingMessage) => {
-    console.log("Hello there");
-    ws.on("message", (msg : RawData)=> {
-        console.log(msg.toString());
+wss.on("connection", (ws: WebSocket, request: IncomingMessage) => {
+    let connection: Connection = {
+        ws: ws,
+        ms: 0,
+        lastPackage: 0,
+        x: 0,
+        y: 0,
+    };
+    ws.send(`${width};${height}`);
+    connections.push(connection);
+    console.log(`${connections.length} connections`);
+    ws.on("message", (msg: RawData) => {
+        if (msg.toString().length == 0)
+            connection.ms = (Date.now() - connection.lastPackage) / 2;
+        else {
+            switch (msg.toString()) {
+                case "LeftArrow":
+                    if (connection.x != 0)
+                        connection.x--;
+                    break;
+                case "UpArrow":
+                    if (connection.y != 0)
+                        connection.y--;
+                    break;
+                case "RightArrow":
+                    if (connection.x != width - 1)
+                        connection.x++;
+                    break;
+                case "DownArrow":
+                    if (connection.y != height - 1)
+                        connection.y++;
+                    break;
+            }
+        }
     });
 
-    ws.on("close", (code : number) => {
-        console.log("Bye bye sucka");
+    ws.on("close", (code: number) => {
+        connections = connections.filter((connection) => connection.ws != ws);
     });
 });
+
+setInterval(() => {
+    let positions = "";
+    for (let connection of connections) {
+        positions += `${connection.x}|${connection.y};`;
+    }
+    for (let connection of connections) {
+        connection.ws.send(positions);
+    }
+    //console.log(`avg ${(avgMS / connections.length).toFixed(2)} ms \n${connections.length} connections`);
+}, 50);
